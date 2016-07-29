137aafe80515f4a6768292da5d2340b922cc38c9
Aaron Job <aaron@gravypower.net>
2016-07-29 19:13:52 +1000

    added in injection for a module

--- src/Furnace.Core.Node.Play/NodeModule.cs
+++ src/Furnace.Core.Node.Play/NodeModule.cs
@@ -1,10 +1,12 @@
-﻿using Furnace.Core.Play.Module;
+﻿using Furnace.Core.Node.Play.Query;
+using Furnace.Core.Play.Module;
+using Furnace.Core.Play.Query;
 
 namespace Furnace.Core.Node.Play
 {
     public sealed class NodeModule: FurnaceModule
     {
-        public NodeModule()
+        public NodeModule(IQueryHandler<NodeQuery, NodeQueryResult> nodeQueryHandeler)
         {
             Get("/node/{nodeId}", parameters => "nodeId is " + parameters.nodeId);
         }
