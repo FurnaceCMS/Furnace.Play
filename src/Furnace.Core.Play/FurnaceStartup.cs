69c61038c192bfa73737b494892b0ebc3bb92d43
Rob Earlam <robert.earlam@gmail.com>
2016-08-21 15:59:44 +1000

    Setting up some basic environment based settings files

--- src/Furnace.Core.Play/FurnaceStartup.cs
+++ src/Furnace.Core.Play/FurnaceStartup.cs
@@ -1,18 +1,40 @@
 ﻿using Furnace.Core.Play.Composition;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Furnace.Core.Play
 {
     public class FurnaceStartup
     {
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public FurnaceStartup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("furnace.json", false, true)
+                .AddJsonFile($"furnace.{env.EnvironmentName}.json", false, true)
+                .AddEnvironmentVariables();
+
+            _configurationRoot = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_configurationRoot);
             services.AddSingleton<IFurnaceCompositionRootBuilder, CompositionRootBuilder>();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            if (env.IsEnvironment("Developement"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
             app.UseFurnace();
         }
